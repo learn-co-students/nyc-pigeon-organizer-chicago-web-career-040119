ed18a7461ef98488300aeeb586014e9f9731e07c
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-03-07 01:17:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,62 +16,20 @@
 #  }
 #}
 
-pigeon_list = {
-  "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms. K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
-}
-#This is heavily borrowed from Blake's lecture & notes, but refactored a bit.
-pigeons.each do |key, hash|
-  # <:color => {:purple => ["Theo", "Peter Jr.", "Lucky"],etc}>
-  hash.each do |attribute, name_array|
-    # <:purple => ["Theo", "Peter Jr.", "Lucky"]>
-    # <"Subway" => ["Theo", "Queenie"]>
-    name_array.each do |name|
-      # <["Theo", "Peter Jr.", "Lucky"]>
-      if pigeons2[name].nil?
-        pigeons2[name] = {}
-        pigeons2[name][:color] = []
-      end
-      if key == :color
-        #for each name in pigeons, if key = :color, shovel the attribute (purple, grey, etc) into the pigeons2 color array
-        pigeons2[name][key] << attribute
-      else
-        #otherwise assign the key(:lives or :gender) to pigeons2 hash and set it equal to the value of that attribute
-        #convert attributes to string!!!!! yayyyyyyyyy!
-        pigeons2[name][key] = attribute.to_s
+def nyc_pigeon_organizer(pigeon_listdata)
+  pigeon_list = {}
+  pigeon_data.each do |key, details|
+    details.each do |attribute, array_of_names|
+      array_of_names.each do |name|
+        if pigeon_list[name].nil?
+           pigeon_list[name] = {}
+           pigeon_list[name][:color] = []
+        end
+        if key == :color
+          pigeon_list[name][key] << attribute
+        else
+          pigeon_list[name][key] = attribute.to_s
+        end
       end
     end
   end
